cc2609cdc31aa171e126b0efe30773100b436bdb
Raphael TISSERAND <rtissera@made-f0Cr2s3.clusters.42paris.fr>
2024-09-23 19:10:02 +0200

    KICK: c'est sense marcher mais pas encore teste, les messages d'erreur ne sont pas bons

--- srcs/commands/channelCommands/Kick.cpp
+++ srcs/commands/channelCommands/Kick.cpp
@@ -8,24 +8,28 @@ bool	kickTests( Server const & server,
 
 	if ( !server.isClientRegistered( nickname ) ) {
 
+		// client.write(ERR_NOSUCHNICK(SERVER_NAME, nickname));
 		std::cout << "Kick error message" << std::endl;
 		return false;
 	}
 
 	if ( !channelArray.isChan( channelName ) ) {
 
+		// client.write(ERR_NOSUCHCHANNEL(SERVER_NAME, channel_name));
 		std::cout << "Kick error message" << std::endl;
 		return false;
 	}
 
 	if ( !channelArray.userInChannel( client, channelName ) ) {
 
+		// client.write(ERR_USERNOTINCHANNEL(SERVER_NAME, nickname, channel_name));
 		std::cout << "Kick error message" << std::endl;
 		return false;
 	}
 
 	if ( !channelArray.isOperator( client, channelName ) {
 
+		// client.write(ERR_CHANOPRIVSNEEDED(SERVER_NAME, channel_name));
 		std::cout << "Kick error message" << std::endl;
 		return false;
 	}
@@ -33,18 +37,16 @@ bool	kickTests( Server const & server,
 	Client	targetedClient = getClient( nickname );
 	if ( channelArray.isOperator( targetedClient, channelName ) {
 
-		std::cout << "Kick error message" << std::endl;
+		std::cout << "Cannot kick channel operator" << std::endl;
 		return false;
 	}
 
 	return true;
 }
 
-void	kickHandler( Server const & server,
-		ChannelArray const & channelArray,
-		std::string const & channelName,
-		Client client ) {
+void	kickHandler( client& client, Channel& channel ) {
 
+	channel.removeUser( client );
 }
 
 void	kick( Server const & server,
@@ -57,11 +59,12 @@ void	kick( Server const & server,
 
 	if ( !( iss >> channelName >> nickname ) || channelName.empty() || nickname.empty() ) {
 
+		// curr_client.write(RPL_KICK(kicker.get_nickname(), kicker.get_username(), channel_name, nickname, "KICK"));
 		std::cout << "Kick error message" << std::endl;
 		return ;
 	}
 
 	if ( !kickTests( server, channelArray, client, channelName, nickname )) { return ; }
 
-	kickHandler( server, channelArray, channelName, getClient( nickname ));
+	kickHandler( server.getClient( nickname ), channelArray.getChannel( channelName ));
 }
